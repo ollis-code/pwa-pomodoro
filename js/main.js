36b08431ddd51612bc901e58586d59cd9390d0bf
Oliver <ollis.mailbox@googlemail.com>
2021-12-03 14:25:27 +0100

    load example

--- js/main.js
+++ js/main.js
@@ -1,38 +1,8 @@
 window.onload = () => {
-    'use strict';
-    //alert("Hi this is me sending something interesting, cOOL!")
+  'use strict';
 
-  // get the form
-  let form = document.forms.my; // <form name="my"> element
-
-  // get the element
-  let elem = form.elements.one; // <input name="one"> element
-
-  //alert(elem.value); // 1
-
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('./sw.js')
-               .then(() => console.log("registerd service worker!"));
-    }
-
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
   }
-
-
-function clickHandler(event) {
-    console.log('Button Clicked');
- }
-
-const btn = document.querySelector('.btn');
-console.log(btn);
-btn.addEventListener('click', clickHandler);
-
-function clickHandlerSubmit(event) {
-  console.log('Submit Clicked');
-  var el = document.getElementById("mail");
-  console.log(el.value);
 }
-
-var form = document.getElementById("submit");
-console.log(form);
-form.addEventListener('click', clickHandlerSubmit);
