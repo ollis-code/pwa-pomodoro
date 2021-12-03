2babba4ed665e73cedbb7d1d827d06ee8bd07818
Oliver <ollis.mailbox@googlemail.com>
2021-12-03 22:52:53 +0100

    filesToCache

--- sw.js
+++ sw.js
@@ -3,7 +3,14 @@ var filesToCache = [
   '/',
   '/index.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+  'images/hello-icon-128.png',
+  'images/hello-icon-144.png',
+  'images/hello-icon-152.png',
+  'images/hello-icon-192.png',
+  'images/hello-icon-256.png',
+  'images/hello-icon-512.png',
+  '/favicon.ico'
 ];
 
 /* Start the service worker and cache all of the app's content */
