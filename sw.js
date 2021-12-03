35a34040daa1cc51c0391c5542504d496aa84cd7
Oliver <ollis.mailbox@googlemail.com>
2021-12-03 22:54:31 +0100

    filesToCache

--- sw.js
+++ sw.js
@@ -4,12 +4,12 @@ var filesToCache = [
   '/index.html',
   '/css/style.css',
   '/js/main.js',
-  'images/hello-icon-128.png',
-  'images/hello-icon-144.png',
-  'images/hello-icon-152.png',
-  'images/hello-icon-192.png',
-  'images/hello-icon-256.png',
-  'images/hello-icon-512.png',
+  '/images/hello-icon-128.png',
+  '/images/hello-icon-144.png',
+  '/images/hello-icon-152.png',
+  '/images/hello-icon-192.png',
+  '/images/hello-icon-256.png',
+  '/images/hello-icon-512.png',
   '/favicon.ico'
 ];
 
